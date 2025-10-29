85517e701f4ffc4cd9de7aa336e54715edb815ec
youmean <69343466+imyoumin@users.noreply.github.com>
2025-10-29 18:24:43 +0900

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://chatbotbackend-9y86.onrender.com/";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://chatbotbackend-9y86.onrender.com";
 export default API_BASE_URL;
